7cbd843b835c23ea6935e43749905668db9b839a
Patrice Tekeda <104334165+patricetekeda@users.noreply.github.com>
2025-10-24 17:59:27 -0400

    fix formatting in Course struct definition

--- internal/courses/types.go
+++ internal/courses/types.go
@@ -1,10 +1,9 @@
 package courses
 
-
 type Course struct {
 	ID          string `json:"id"`
 	Title       string `json:"title"`
 	Category    string `json:"category"`
-	Level	   string `json:"level"`
+	Level       string `json:"level"`
 	Description string `json:"description"`
 }
